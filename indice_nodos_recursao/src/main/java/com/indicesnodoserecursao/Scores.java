273643801a884c1cbefe3b40274b630b3dcb3aa5
Pablo Zalem <zalem.pablo@yahoo.com>
2026-03-30 20:22:41 -0300

    add and remove

--- indice_nodos_recursao/src/main/java/com/indicesnodoserecursao/Scores.java
+++ indice_nodos_recursao/src/main/java/com/indicesnodoserecursao/Scores.java
@@ -5,7 +5,7 @@ public class Scores {
     private GameEntry[] entries;
     private int numEntries;
 
-    public void Scores() {
+    public Scores() {
         entries = new GameEntry[MAX_SIZE];
         numEntries = 0;
     }
@@ -22,11 +22,33 @@ public class Scores {
         }
 
         int i = numEntries - 1;
+
         for (; (i >= 1) && (newScore > entries[i - 1].getScore()); i--) {
             entries[i] = entries[i - 1];
-            entries[i] = e;
         }
+
+        entries[i] = e;
     }
+
+   public GameEntry remove(int i) throws IndexOutOfBoundsException {
+        if((i < 0) || (i >= numEntries)) {
+            throw new IndexOutOfBoundsException("Invalid index: " + i);
+        }
+
+        GameEntry temp = entries[i];
+
+        for (int j = i; j < numEntries - 1; j++) {
+            entries[j] = entries[j + 1];
+        }
+
+        entries[numEntries - 1] = null;
+        numEntries--;
+
+        return temp;
+
+
+        
+   }
 
     @Override
     public String toString() {
@@ -38,7 +60,7 @@ public class Scores {
             }
             s += entries[i];
         }
-
-        return s = "]";
+        s += "]";
+        return s;
     }
 }
